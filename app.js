1d4da516166706061a0c6f7689a2abba71bd5722
eho <edmundho9@gmail.com>
2021-02-11 21:34:04 -0800

    extract helper methods

--- app.js
+++ app.js
@@ -11,49 +11,46 @@ const ARKK = 'https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_INNOVA
 const ARKF = 'https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_FINTECH_INNOVATION_ETF_ARKF_HOLDINGS.csv';
 const ARKG = 'https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_GENOMIC_REVOLUTION_MULTISECTOR_ETF_ARKG_HOLDINGS.csv';
 
-app.get('/funds/arkw', (req, res) => {
-  const stream = request.get(ARKW).on('error', err => console.log(`Error: ${err}`));
+function createCsvStream(url) {
+  return request.get(url).on('error', err => console.log(`Error: ${err}`));
+}
 
+function sendJsonPayloadFromCsvStream(stream, res) {
   const output = [];
+
   stream.pipe(parser())
   .on('data', row => output.push(row))
   .on('end', () => res.send(output))
+}
+
+app.get('/funds/arkw', (req, res) => {
+  const stream = createCsvStream(ARKW);
+
+  sendJsonPayloadFromCsvStream(stream, res);
 })
 
 app.get('/funds/arkk', (req, res) => {
-  const stream = request.get(ARKK).on('error', err => console.log(`Error: ${err}`));
+  const stream = createCsvStream(ARKK);
 
-  const output = [];
-  stream.pipe(parser())
-  .on('data', row => output.push(row))
-  .on('end', () => res.send(output))
+  sendJsonPayloadFromCsvStream(stream, res);
 })
 
 app.get('/funds/arkg', (req, res) => {
-  const stream = request.get(ARKG).on('error', err => console.log(`Error: ${err}`));
+  const stream = createCsvStream(ARKG);
 
-  const output = [];
-  stream.pipe(parser())
-  .on('data', row => output.push(row))
-  .on('end', () => res.send(output))
+  sendJsonPayloadFromCsvStream(stream, res);
 })
 
 app.get('/funds/arkq', (req, res) => {
-  const stream = request.get(ARKQ).on('error', err => console.log(`Error: ${err}`));
+  const stream = createCsvStream(ARKQ);
 
-  const output = [];
-  stream.pipe(parser())
-  .on('data', row => output.push(row))
-  .on('end', () => res.send(output))
+  sendJsonPayloadFromCsvStream(stream, res);
 })
 
 app.get('/funds/arkf', (req, res) => {
-  const stream = request.get(ARKF).on('error', err => console.log(`Error: ${err}`));
+  const stream = createCsvStream(ARKF);
 
-  const output = [];
-  stream.pipe(parser())
-  .on('data', row => output.push(row))
-  .on('end', () => res.send(output))
+  sendJsonPayloadFromCsvStream(stream, res);
 })
 
 app.listen(port, () => {
